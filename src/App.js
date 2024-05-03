8dc99d825bfce70c5fb09a7358755f9ef1d30d62
BakariMorris <bakari_morris@yahoo.com>
2024-05-03 05:16:56 -0400

    banking overhaul

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import MainContainer from './hoc/MainContainer/MainContainer.js';
 import Header from './components/Header/Header.js';
 import Wrapper from './hoc/Wrapper/Wrapper.js';
+import LeftSide from './components/LeftSide/LeftSide.js';
+import UserCard1 from './components/UserCards/UserCard1.js';
+import UserCard2 from './components/UserCards/UserCard2.js';
 
 import './App.scss';
 
 function App() {
   return (
     <Wrapper>
+      <LeftSide/>
       <MainContainer>
-        <Header></Header>
+        <Header/>
+        <UserCard1/>
+        <UserCard2/>
       </MainContainer>
     </Wrapper>
   );
